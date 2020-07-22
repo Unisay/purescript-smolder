6c5f6004a35ffd3443a0a4e0175f1b5284e1e0a1
Yuriy Lazarev <unisay@users.noreply.github.com>
2020-07-22 21:05:20 +0200

    renderWithEvents

--- src/Text/Smolder/Renderer/String.purs
+++ src/Text/Smolder/Renderer/String.purs
@@ -1,19 +1,19 @@
-module Text.Smolder.Renderer.String
-  ( render
-  ) where
+module Text.Smolder.Renderer.String (renderWithEvents, render) where
 
 import Prelude
-
 import Control.Comonad (extend)
 import Control.Comonad.Cofree (Cofree, head, mkCofree, tail, (:<))
 import Control.Monad.Free (foldFree)
 import Control.Monad.State (State, evalState, execState, get, put, state)
 import Data.Array ((..))
 import Data.CatList (CatList)
+import Data.CatList as CL
 import Data.Char (toCharCode)
-import Data.Foldable (elem, fold, foldr)
+import Data.Foldable (elem, fold, foldr, surround)
+import Data.Int (Parity(..))
 import Data.Map (Map, lookup, fromFoldable)
 import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Monoid (guard)
 import Data.Set (Set)
 import Data.Set as Set
 import Data.String (length)
@@ -21,45 +21,54 @@ import Data.String.CodeUnits (fromCharArray, toCharArray)
 import Data.Tuple (Tuple(..))
 import Data.Tuple.Nested ((/\))
 import Global.Unsafe (unsafeEncodeURI)
-import Text.Smolder.Markup (Attr(..), Markup, MarkupM(..), NS(..))
+import Text.Smolder.Markup
+  ( Attr(..)
+  , Markup
+  , MarkupM(..)
+  , NS(..)
+  , EventHandler(..)
+  )
 
 escapeMap :: Map Char String
-escapeMap = fromFoldable
-  [ '&' /\ "&amp;"
-  , '<' /\ "&lt;"
-  , '>' /\ "&gt;"
-  , '"' /\ "&quot;"
-  , '\'' /\ "&#39;"
-  , '/' /\ "&#x2F;"
-  ]
+escapeMap =
+  fromFoldable
+    [ '&' /\ "&amp;"
+    , '<' /\ "&lt;"
+    , '>' /\ "&gt;"
+    , '"' /\ "&quot;"
+    , '\'' /\ "&#39;"
+    , '/' /\ "&#x2F;"
+    ]
 
 escapeMIMEMap :: Map Char String
-escapeMIMEMap = fromFoldable
-  [ '&' /\ "&amp;"
-  , '<' /\ "&lt;"
-  , '"' /\ "&quot;"
-  , '\'' /\ "&#39;"
-  ]
+escapeMIMEMap =
+  fromFoldable
+    [ '&' /\ "&amp;"
+    , '<' /\ "&lt;"
+    , '"' /\ "&quot;"
+    , '\'' /\ "&#39;"
+    ]
 
 voidElements :: Set String
-voidElements = Set.fromFoldable
-  [ "area"
-  , "base"
-  , "br"
-  , "col"
-  , "command"
-  , "embed"
-  , "hr"
-  , "img"
-  , "input"
-  , "keygen"
-  , "link"
-  , "meta"
-  , "param"
-  , "source"
-  , "track"
-  , "wbr"
-  ]
+voidElements =
+  Set.fromFoldable
+    [ "area"
+    , "base"
+    , "br"
+    , "col"
+    , "command"
+    , "embed"
+    , "hr"
+    , "img"
+    , "input"
+    , "keygen"
+    , "link"
+    , "meta"
+    , "param"
+    , "source"
+    , "track"
+    , "wbr"
+    ]
 
 isMIMEAttr :: String -> String -> Boolean
 isMIMEAttr tag attr
@@ -102,71 +111,98 @@ toStream s = foldr (\c t -> c :< (Just t)) (mkCofree '\x0' Nothing) cs
 fromStream :: Cofree Maybe String -> String
 fromStream = go ""
   where
-    go :: String -> Cofree Maybe String -> String
-    go result cof =
-      case (head cof), (tail cof) of
-        _, Nothing -> result
-        s, (Just cof') -> go (result <> s) cof'
+  go :: String -> Cofree Maybe String -> String
+  go result cof = case (head cof), (tail cof) of
+    _, Nothing -> result
+    s, (Just cof') -> go (result <> s) cof'
 
 escape :: Map Char String -> String -> String
 escape m = fromStream <<< extend escapeS <<< toStream
   where
-    startsEntity :: Maybe (Cofree Maybe Char) -> Boolean
-    startsEntity (Just w) =
-              case head w, tail w of
-                '#',  Just w' -> checkTail (48..57) w'
-                '#',  Nothing -> false
-                _,    _       -> checkTail (65..90 <> 97..122) w
-    startsEntity Nothing = false
+  startsEntity :: Maybe (Cofree Maybe Char) -> Boolean
+  startsEntity (Just w) = case head w, tail w of
+    '#', Just w' -> checkTail (48 .. 57) w'
+    '#', Nothing -> false
+    _, _ -> checkTail (65 .. 90 <> 97 .. 122) w
 
-    checkTail :: Array Int -> Cofree Maybe Char -> Boolean
-    checkTail allowed = flip evalState false <<< checkTail'
-      where
-        -- keep if `;` is allowed in `State` monad
-        checkTail' :: Cofree Maybe Char -> State Boolean Boolean
-        checkTail' w =
-          case toCharCode $ head w of
-            cc  | cc `elem` allowed -> do
-                    put true
-                    fromMaybe (pure false) $ checkTail' <$> tail w
-                | cc == 59 -> get
-                | otherwise -> pure false
+  startsEntity Nothing = false
 
-    escapeS :: Cofree Maybe Char -> String
-    escapeS w =
-      case head w of
-        '&' | startsEntity (tail w) -> "&"
-            | otherwise             -> "&amp;"
-        c                           -> fromMaybe (fromCharArray [c]) $ lookup c m
+  checkTail :: Array Int -> Cofree Maybe Char -> Boolean
+  checkTail allowed = flip evalState false <<< checkTail'
+    where
+    -- keep if `;` is allowed in `State` monad
+    checkTail' :: Cofree Maybe Char -> State Boolean Boolean
+    checkTail' w = case toCharCode $ head w of
+      cc
+        | cc `elem` allowed -> do
+          put true
+          fromMaybe (pure false) $ checkTail' <$> tail w
+        | cc == 59 -> get
+        | otherwise -> pure false
+
+  escapeS :: Cofree Maybe Char -> String
+  escapeS w = case head w of
+    '&'
+      | startsEntity (tail w) -> "&"
+      | otherwise -> "&amp;"
+    c -> fromMaybe (fromCharArray [ c ]) $ lookup c m
 
 escapeAttrValue :: String -> String -> String -> String
 escapeAttrValue tag key value
-  | isURLAttr tag key   = unsafeEncodeURI value
-  | isMIMEAttr tag key  = escape escapeMIMEMap value
-  | otherwise           = escape escapeMap value
+  | isURLAttr tag key = unsafeEncodeURI value
+  | isMIMEAttr tag key = escape escapeMIMEMap value
+  | otherwise = escape escapeMap value
 
-showAttrs :: String -> CatList Attr → String
+showAttrs :: String -> CatList Attr -> String
 showAttrs tag = map showAttr >>> fold
   where
-    showAttr (SafeAttr key value) = " " <> key <> "=\"" <> value <> "\""
-    showAttr (Attr key value) = " "
+  showAttr (SafeAttr key value) = " " <> key <> "=\"" <> value <> "\""
+
+  showAttr (Attr key value) =
+    " "
       <> key
       <> "=\""
       <> escapeAttrValue tag key value
       <> "\""
 
-renderItem :: ∀ e. MarkupM e ~> State String
-renderItem (Element ns name children attrs _ rest) =
-  let c = render children
-      b = "<" <> name <> showAttrs name attrs <>
-          (if length c > 0 || (ns == HTMLns && not Set.member name voidElements)
-           then ">" <> c <> "</" <> name <> ">"
-           else "/>")
-  in state \s → Tuple rest $ append s b
-renderItem (Content text rest) = state \s → Tuple rest $ append s $ escape escapeMap text
-renderItem (Doctype text rest) = state \s → Tuple rest $ append s $ "<!DOCTYPE " <> text <> ">"
-renderItem (Empty rest) = pure rest
+renderItem :: ∀ e. (EventHandler e -> String) -> MarkupM e ~> State String
+renderItem er (Element ns name children attrs eventHandlers rest) =
+  let
+    c = renderWithEvents er children
+
+    b =
+      "<" <> name <> showAttrs name attrs
+        <> guard (CL.length eventHandlers > 0)
+            (surround " " $ renderEventHandler <$> eventHandlers)
+        <> ( if length c > 0 || (ns == HTMLns && not Set.member name voidElements) then
+              ">" <> c <> "</" <> name <> ">"
+            else
+              "/>"
+          )
+  in
+    state \s -> Tuple rest $ append s b
+  where
+  renderEventHandler h@(EventHandler eventName event) =
+    eventName
+      <> "="
+      <> "\""
+      <> escape escapeMap (er h)
+      <> "\""
+
+renderItem _ (Content text rest) =
+  state \s ->
+    Tuple rest $ append s $ escape escapeMap text
+
+renderItem _ (Doctype text rest) =
+  state \s ->
+    Tuple rest $ append s $ "<!DOCTYPE " <> text <> ">"
+
+renderItem _ (Empty rest) = pure rest
+
+-- | Render markup as an HTML string with event renderer
+renderWithEvents :: ∀ e. (EventHandler e -> String) -> Markup e -> String
+renderWithEvents er m = execState (foldFree (renderItem er) m) ""
 
 -- | Render markup as an HTML string.
-render :: ∀ e. Markup e → String
-render f = execState (foldFree renderItem f) ""
+render :: ∀ e. Markup e -> String
+render = renderWithEvents (const mempty)
